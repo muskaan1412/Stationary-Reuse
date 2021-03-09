e3118e4b95f2da7bb7c635f756d40d50529fefd7
anujsachdeva10 <anujsachdeva10@gmail.com>
2021-03-09 18:11:49 +0530

    edit profile page added

--- StationaryReuse/StationaryReuse/urls.py
+++ StationaryReuse/StationaryReuse/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +26,4 @@ urlpatterns = [
     path('registration/', views.UserRegistrationView, name="user_registration"),
     path('login/', views.UserLoginView, name="user_login"),
     path('user/', include('myapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- StationaryReuse/myapp/urls.py
+++ StationaryReuse/myapp/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'user'
 
 urlpatterns = [
-    path('<str:username>', views.UserProfileView, name="profile")
-]
+    path('<str:username>/', views.UserProfileView, name="profile"),
+    path('<str:username>/editprofile/', views.UserEditProfileView, name="edit_profile")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
